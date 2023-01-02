eef30800f85f8140041d22ba534ff3edfd038e33
KarloLeksic <lekxka@gmail.com>
2023-01-02 11:36:12 +0100

    Added first version of the library

---
+++ src/SCD-4X-SOLDERED.cpp
@@ -0,0 +1,20 @@
+/**
+ **************************************************
+ *
+ * @file        SCD-4X-SOLDERED.cpp
+ * @brief       Soldered SCD-4X Arduino library.
+ *
+ *
+ * @copyright   GNU General Public License v3.0
+ * @authors     Karlo Leksic for soldered.com
+ ***************************************************/
+
+
+#include "SCD-4X-SOLDERED.h"
+
+/**
+ * @brief                   Sensor specific native constructor.
+ */
+SCD_4X::SCD_4X(scd4x_sensor_type_e sensorType)
+{    
+}
