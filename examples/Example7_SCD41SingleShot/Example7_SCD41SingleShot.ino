226bb70d20ab3b1c1ff98bbff500681bf456608a
Main Push Robot <robot@example.com>
2023-01-02 11:00:37 +0000

    Committing dev changes

--- examples/Example7_SCD41SingleShot/Example7_SCD41SingleShot.ino
+++ examples/Example7_SCD41SingleShot/Example7_SCD41SingleShot.ino
@@ -1,21 +1,22 @@
-/*
-  SCD41 Low Power Single Shot
-  By: Paul Clark
-  Based on earlier code by: Nathan Seidle
-  Modified by Soldered
-
-  License: MIT. See license file for more information but you can
-  basically do whatever you want with this code.
-
-  solde.red/333191, solde.red/333220
-
-  This example prints the current CO2 level, relative humidity, and temperature in C.
-
-  Hardware Connections:
-  Attach Dasduino to computer using a USB cable.
-  Connect SCD40/41 to Dasduino using easyC cable.
-  Open Serial Monitor at 115200 baud.
-*/
+/**
+ * @file          Example1_BasicReadings.ino
+ * 
+ * @brief         SCD41 Low Power Single Shot. This example prints the current CO2 level, relative humidity, and temperature in C.
+ * 
+ * @author        Paul Clark
+ *                Based on earlier code by: Nathan Seidle
+ *                Modified by Soldered
+ * 
+ * @link          solde.red/333191, solde.red/333220
+ * 
+ * @note          Hardware Connections:
+ *                Attach Dasduino to computer using a USB cable.
+ *                Connect SCD40/41 to Dasduino using easyC cable.
+ *                Open Serial Monitor at 115200 baud.
+ *
+ * License: MIT. See license file for more information but you can
+ * basically do whatever you want with this code.
+ */
 
 #include <Wire.h>
 
